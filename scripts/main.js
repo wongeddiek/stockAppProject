fd9fd1f3171e1e0c9c56df97be733f79462a5ac2
Edward Wong <wong.eddiek@gmail.com>
2017-08-10 10:51:20 -0600

    Add js code comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// User profile object constructor
 function User(id, email, password, first, last){
   this.id = id;
   this.email = email;
@@ -6,6 +7,7 @@ function User(id, email, password, first, last){
   this.last = last;
 }
 
+// User Account object constructor
 function Account(id, ticker, fundname, category, share, price, balance) {
   this.id = id;
   this.ticker = ticker;
@@ -16,8 +18,10 @@ function Account(id, ticker, fundname, category, share, price, balance) {
   this.balance = balance;
 }
 
+// Creates sample user profile
 var jdoe = new User(1, "jdoe@example.com", "123456", "Joe", "Doe")
 
+// Creates sample user account
 var jdoe1 = new Account(1, "VBMFX", "Vanguard Total Bond Market Index", "Bond", 10.81, 370.028, 4000);
 
 var jdoe2 = new Account (1, "VTSMX", "Vanguard Total Stock Market Index", "Large Cap", 61.63, 32.452, 2000)
@@ -30,6 +34,7 @@ var jdoe5 = new Account(1, "VGSTX", "Vanguard International Index", "Internation
 
 var jdoeAccount = [jdoe1, jdoe2, jdoe3, jdoe4, jdoe5];
 
+// function to sum all investment balances
 function totalBalance(account) {
   var total = 0;
   for (var i = 0; i < account.length; i++) {
@@ -38,9 +43,10 @@ function totalBalance(account) {
   return total;
 }
 
+// creates sample user total account balance
 var jdoeBalance = totalBalance(jdoeAccount).toFixed(2);
 
-
+// creates and format current date in MMM dd yyyy format
 var date = new Date();
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 var fullDate = months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
