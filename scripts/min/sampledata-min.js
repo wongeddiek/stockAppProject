29fcc3eb0faec78a815b126f1212ec2ff7ae0c7f
Edward Wong <wong.eddiek@gmail.com>
2017-08-10 13:40:19 -0600

    moved sample data js to its own js file

---
+++ scripts/min/sampledata-min.js
@@ -0,0 +1,37 @@
+// User profile object constructor
+function User(id, email, password, first, last){
+  this.id = id;
+  this.email = email;
+  this.password = password;
+  this.first = first;
+  this.last = last;
+}
+
+// User Account object constructor
+function Account(id, ticker, fundname, category, share, price, balance) {
+  this.id = id;
+  this.ticker = ticker;
+  this.fundname = fundname;
+  this.category = category;
+  this.price = price;
+  this.share = share;
+  this.balance = balance;
+}
+
+// Creates sample user profile
+var user = new User(1, "jdoe@example.com", "123456", "Joe", "Doe")
+
+// Creates sample user account
+var user1 = new Account(1, "VBMFX", "Vanguard Total Bond Market Index", "Bond", 10.81, 370.028, 4000);
+
+var user2 = new Account (1, "VTSMX", "Vanguard Total Stock Market Index", "Large Cap", 61.63, 32.452, 2000)
+
+var user3 = new Account(1, "VIMSX", "Vanguard Mid-Cap Index", "Mid Cap", 39.27, 38.197, 1500)
+
+var user4 = new Account(1, "NAESX", "Vanguard Small-Cap Index", "Small Cap", 64.69, 15.458, 1000)
+
+var user5 = new Account(1, "VGSTX", "Vanguard International Index", "International", 26.21, 57.230, 1500)
+
+var userAccount = [user1, user2, user3, user4, user5];
+
+
