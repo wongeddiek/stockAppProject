93fb21050adc138b74106d3c6ee4fcce2b1f92fd
Edward Wong <wong.eddiek@gmail.com>
2017-08-17 16:47:33 -0600

    created Node server and dependencies

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+var request = require('request')
+var bodyParser = require('body-parser')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json())
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 3000!')
+})
